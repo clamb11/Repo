f726eea1d19ab9fc5686430382876e633b127d7c
clamb11 <123419865+clamb11@users.noreply.github.com>
2023-12-18 15:51:49 -0700

    Update WeatherApp.java

--- Weather/src/WeatherApp.java
+++ Weather/src/WeatherApp.java
@@ -1,8 +1,17 @@
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class WeatherApp extends JFrame {
     JTextField zipCodeSearchBar;
@@ -78,17 +87,65 @@ public class WeatherApp extends JFrame {
         setLocationRelativeTo(null);
     }
 
-    private Integer getWeatherData(String zipCode, String countryCode) {
+    private Map getWeatherData(String zipCode, String countryCode) throws IOException {
         // TODO: Implement the code to fetch weather data from your preferred API
-        String APIKey = "4d5bfce61bb0bfcb407e7e53221ed5d0";
+        String APIKey = "";
         //get lat and long for weather call
-        //response1 = 'http://api.openweathermap.org/geo/1.0/zip?zip=zipCode,countryCode&appid={API key}';
-        String response1 = null;
-        Double lat = Double.parseDouble(response1.split("\"lat\": ")[1].split(",")[0]);
-        Double lon = Double.parseDouble(response1.split("\"lon\": ")[1].split(",")[0]);
-        //http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API key}
-        int currData = 0;
-        return currData; // Dummy data
+        URL latlogurl = new URL("http://api.openweathermap.org/geo/1.0/zip?zip="+zipCode+","+countryCode+"&appid="+APIKey);
+        HttpURLConnection con = (HttpURLConnection) latlogurl.openConnection();
+        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        if(responseCode == HttpURLConnection.HTTP_OK){
+            BufferedReader reader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            StringBuilder response1 = new StringBuilder();
+            String line;
+            while((line = reader.readLine())!= null){
+                response1.append(line);
+            }
+            reader.close();
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode jsonNode = objectMapper.readTree(response1.toString());
+
+            // Extract relevant information
+            double lat = jsonNode.get("lat").asDouble();
+            double lon = jsonNode.get("lon").asDouble();
+            URL weatherURL = new URL("https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+lon+"&limit=1&appid="+APIKey+"&units=imperial");
+            HttpURLConnection con2 = (HttpURLConnection) weatherURL.openConnection();
+            con.setRequestMethod("GET");
+            int responseCode2 = con2.getResponseCode();
+            if(responseCode2 == HttpURLConnection.HTTP_OK){
+                BufferedReader reader2 = new BufferedReader(new InputStreamReader(con2.getInputStream()));
+                StringBuilder response2 = new StringBuilder();
+                String line2;
+                while((line2 = reader.readLine())!= null){
+                    response2.append(line2);
+                }
+                reader2.close();
+                ObjectMapper objectMapper2 = new ObjectMapper();
+                JsonNode jsonNode2 = objectMapper2.readTree(response2.toString());
+
+                // Extract relevant information
+                double temp = jsonNode.get("main").get("temp").asDouble();
+                double feelsLike = jsonNode.get("main").get("feels_like").asDouble();
+                double tempMin = jsonNode.get("main").get("temp_min").asDouble();
+                double tempMax = jsonNode.get("main").get("temp_max").asDouble();
+                String weather = jsonNode.get("weather").get("description").asText();
+                double windSpeed = jsonNode.get("wind").get("speed").asDouble();
+
+                Map<String, Object> currData = new HashMap<>();
+                currData.put("Temp", temp);
+                currData.put("Feels like", feelsLike);
+                currData.put("Temp Min", tempMin);
+                currData.put("Temp Max", tempMax);
+                currData.put("Weather", weather);
+                currData.put("Wind Speed", windSpeed);
+                if(windSpeed > 10.8){
+                    currData.put("Windy?", "yes");
+                }
+                return currData;
+            }
+
+        return null;
     }
 
     private String suggestClothing(double temperature) {
